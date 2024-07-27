1d3d4259da17c41518eeecc9eeec7b59698d3ecb
Mohamed-Sharaf <61689053+Mohamed-Sharaf@users.noreply.github.com>
2024-07-27 04:05:50 +0300

    Add files via upload

---
+++ ALU_UVM/intf.sv
@@ -0,0 +1,13 @@
+`ifndef Guard_INTF
+`define Guard_INTF
+
+interface intf (input bit clock);
+  logic [7:0] A, B;
+  logic [2:0] op;
+  logic reset_n;
+  logic start;
+  logic done;
+  logic [15:0] result;
+endinterface
+
+`endif // Guard_INTF
