a9e3bd16164ddfb5754f6556b07666572e3b5740
Mohamed-Sharaf <61689053+Mohamed-Sharaf@users.noreply.github.com>
2024-08-07 18:42:28 +0300

    Update ALU_config.sv

--- ALU_UVM/ALU_config.sv
+++ ALU_UVM/ALU_config.sv
@@ -1,3 +1,20 @@
+//-------------------------------------------------------------------------------------------------
+//
+//                                 ALU VERIFICATION
+//
+//-------------------------------------------------------------------------------------------------
+// CREATOR    : Mohamed Sharaf
+// PROJECT    : ALU UVM TEST BENCH
+//-------------------------------------------------------------------------------------------------
+// TITLE      : ALU Configuration  
+// DESCRIPTION: THIS BLOCK INCLUDES THE WHOLE ENVIRONMENT CONFIGURATIONS THAT ARE SET IN THE TEST
+//-------------------------------------------------------------------------------------------------
+// LOG DETAILS
+//-------------
+// VERSION          NAME            DATE        DESCRIPTION
+//    1         Mohamed Sharaf    07082024     FILE CREATION
+//-------------------------------------------------------------------------------------------------
+
 class ALU_configuration extends uvm_object;
 	`uvm_object_utils(ALU_configuration)
 
